eaed1793228788faa191b7e36ab945f34c5b7270
Agustin <agustin@Agustins-MacBook-Pro.local>
2024-11-18 11:37:42 -0300

    HomePage avance

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AuthPage from './Pages/LoginForm.jsx'
-
+import AuthPage from './Pages/LoginForm.jsx';
+import HomePage from './Pages/Home.jsx';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +10,19 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="AuthPage">
-        <Stack.Screen name="AuthPage" component={AuthPage} options={{ title: 'Auth' }} />
-
+        {/* Pantalla de Autenticación 
+        <Stack.Screen 
+          name="AuthPage" 
+          component={AuthPage} 
+          options={{ title: 'Auth', headerShown: false }} 
+        />
+        */}
+        {/* Pantalla Home */}
+        <Stack.Screen 
+          name="HomePage" 
+          component={HomePage} 
+          options={{ title: 'Home', headerShown: false }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
